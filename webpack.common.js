d3112137ae6361c52c28d7338b0ec1778241013a
halawa_oji97 <ozidhalawa97@gmail.com>
2021-07-15 22:26:37 +0700

    change webpack file

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ const path = require('path');
 module.exports = {
 	entry: path.resolve(__dirname, 'src/scripts/index.js'),
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'build'),
 		filename: 'bundle.js',
 	},
 	module: {
